97589173add74a215bbf454525552d192e302da2
terasi <hitokunn750@gmail.com>
2024-08-10 19:20:31 +0900

    java text13

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("例外が\r\n発生しました。");
-
+		for(int i=0; i<=5; i++){
+			String b = i%2 == 0 ? "☆":"★";
+			System.err.println(b);
+		}
 	}
 
 }
